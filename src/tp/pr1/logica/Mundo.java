71b1e43613803f108add6e0fcb50bef21284578a
german6 <german6@users.noreply.github.com>
2015-10-23 01:25:46 +0200

    Añadir, eliminar y mover celulas

--- src/tp/pr1/logica/Mundo.java
+++ src/tp/pr1/logica/Mundo.java
@@ -10,19 +10,31 @@ public class Mundo {
 	private Superficie superficie = new Superficie(N_FILAS,N_COLUMNAS);
 	
 	public void evoluciona() {
-		
+		//Evolucionar aquí <---
 	}
 	
+	//Crea una nueva célula en el mundo
 	public boolean crearCelula(int fila, int columna) {
-		return this.superficie.insertarCelula(fila, columna);
+		return this.superficie.insertarCelula(new Celula(), fila, columna);
 	}
 	
+	//Mueve una célula si la posición está disponible
+	public boolean moverCelula(int f1, int c1, int f2, int c2) {
+			if(this.superficie.posDisponible(f2,c2)) {			
+			Celula cel = this.superficie.getCelula(f1, c1);
+			this.superficie.insertarCelula(cel, f2, c2);
+			this.superficie.eliminarCelula(c1, c1);
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	//Devuelve un entero aleatorio entre min y max
 	int numAleatorio(int min, int max) {
-		if(min<max){
+		if(min<max)
 			return (int)Math.random()*(max-min+1)+min;	
-		}
-		else {
+		else
 			return 0;
-		}
 	}
 }
--- src/tp/pr1/logica/Superficie.java
+++ src/tp/pr1/logica/Superficie.java
@@ -5,19 +5,35 @@ public class Superficie {
 	private int filas;
 	private int columnas;
 	
-	//constructora de la superficie
+	//Constructor de la superficie
 	public Superficie(int nf, int nc){
 		this.filas=nf;
 		this.columnas=nc;
 		this.superficie = new Celula[nf][nc];
 	}
 	
-	public boolean insertarCelula(int fila, int columna){
-		boolean vacio = this.superficie[fila][columna]==null;
+	//Comprueba la disponibilidad de una casilla
+	public boolean posDisponible(int fila, int columna) {
+		return this.superficie[fila][columna] != null;
+	}
+	
+	//Inserta una célula en una posición del tablero
+	public boolean insertarCelula(Celula celula, int fila, int columna){
+		boolean vacio = this.posDisponible(fila,columna);
 		if(vacio){
-			this.superficie[fila][columna] = new Celula();
+			this.superficie[fila][columna] = celula;
 		}
 		return vacio;
+	}
+	
+	//Elimina una célula de una posición del tablero
+	public void eliminarCelula(int fila, int columna){
+		this.superficie[fila][columna] = null;
+	}
+	
+	//Getter de de células
+	public Celula getCelula(int fila, int col) {
+		return this.superficie[fila][col];
 	}
 	
 }
