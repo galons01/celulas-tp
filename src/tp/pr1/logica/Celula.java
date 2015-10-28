dd29cc0918af76725d8ca1d9199fb44e3bc9f186
galons01 <galons01@ucm.es>
2015-10-28 10:17:25 +0100

    añadido celulas

--- src/tp/pr1/logica/Celula.java
+++ src/tp/pr1/logica/Celula.java
@@ -1,6 +1,17 @@
 package tp.pr1.logica;
 
 public class Celula {
-	private int pasosSinMoverse;
-	private int pasosDados;
+	private int turnosSinMover = 0;
+	private int pasos = 0;
+	
+	static private final short MAX_PASOS_SIN_MOVER = 3;
+	static private final short PASOS_REPRODUCCION = 6;
+	
+	
+	
+	
+	public int pasosDados() { 
+		return this.pasos;
+	}
+	
 }
