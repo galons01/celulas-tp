0eba3f717c92d07dc1cb9da9572278ec9de7e25b
german6 <dev.germanfr@gmail.com>
2015-11-03 11:00:00 +0100

    Implementados getters y setters de las clase Celula

--- src/tp/pr1/logica/Celula.java
+++ src/tp/pr1/logica/Celula.java
@@ -1,17 +1,41 @@
 package tp.pr1.logica;
 
 public class Celula {
-	private int turnosSinMover = 0;
-	private int pasos = 0;
+	private static final short MAX_PASOS_SIN_MOVER = 3;
+	private static final short PASOS_REPRODUCCION = 4;
+
+	private short pasosReprod;	//Pasos que le quedan para dividirse (pasos movidos)
+	private short pasosMuerte;	//Pasos que le quedan para morir (sin moverse)
 	
-	static private final short MAX_PASOS_SIN_MOVER = 3;
-	static private final short PASOS_REPRODUCCION = 6;
+	public Celula() {
+		this.pasosReprod = Celula.PASOS_REPRODUCCION;
+		this.pasosMuerte = Celula.MAX_PASOS_SIN_MOVER;
+	}
 	
+	public short getPasosReprod() { 
+		return this.pasosReprod;
+	}
+
+	public short getPasosMuerte() {
+		return this.pasosMuerte;
+	}
+
+	public boolean darPaso() {
+		if(this.pasosReprod <= 0)
+			return false;
+		else {
+			this.pasosReprod--;
+			return true;
+		}
+	}
 	
-	
-	
-	public int pasosDados() { 
-		return this.pasos;
+	public boolean estarQuieta() {
+		if(this.pasosMuerte <= 0)
+			return false;
+		else {
+			this.pasosMuerte--;
+			return true;
+		}
 	}
 	
 }
