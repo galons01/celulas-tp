34649480aa9bc1dffba3a7e30b2d40f92dd7bd1d
Guillermo <galons01@ucm.es>
2015-11-13 14:36:01 +0100

    comentarios de casilla

--- src/tp/pr1/logica/Casilla.java
+++ src/tp/pr1/logica/Casilla.java
@@ -1,22 +1,23 @@
 package tp.pr1.logica;
 
 public class Casilla {
-	private int fila = 0;
-	private int col = 0;
+	private int fila = 0; 	/*fila en la que se posiciona*/
+	private int col = 0;	/*columna en la que se posiciona*/
 	
 	public Casilla (int f, int c) {
 		this.fila = f;
 		this.col = c;
 	}
+	// copia una casilla 
 	public void copiar(Casilla c) {
 		c.fila = this.fila;
 		c.col = this.col;
 	}
-
+	//  obtener la columna
 	public int getColumna() {
 		return this.col;
 	}
-	
+	//  obtiene la fila
 	public int getFila() {
 		return this.fila;
 	}
